6470ceb66468c301af19a4d00b584ccd37468a8f
TatianaBerglund <35171106+TatianaBerglund@users.noreply.github.com>
2018-05-21 21:46:57 +0200

    Initial commit

---
+++ CoolBooks/Controllers/BaseController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using CoolBooks.Models;
+
+namespace CoolBooks.Controllers
+{
+    public class BaseController : Controller
+    {
+        private coolbooks db = new coolbooks();
+        // GET: Base
+        protected override void OnActionExecuting(ActionExecutingContext filterContext)
+        {
+            base.OnActionExecuting(filterContext);
+            ViewData["Genres"] = db.Genres.ToList();
+        }
+    }
+}
---
+++ CoolBooks/Controllers/HomeController.cs
@@ -0,0 +1,40 @@
+﻿using CoolBooks.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CoolBooks.Controllers
+{
+    public class HomeController : BaseController
+    {
+        private coolbooks db = new coolbooks();
+        public ActionResult Index()
+        {
+          
+            var books = db.Books.OrderByDescending(b => b.Created).Where(b => b.IsDeleted == false);
+            ViewData["books"] = books;
+            return View();
+        }
+
+        public ActionResult About()
+        {
+           
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+          
+            ViewBag.Message = "Your contact page.";
+            var Genres = db.Genres.OrderByDescending(g => g.Name).Where(g => g.IsDeleted == false);
+            return View(Genres.ToList());
+            
+
+            
+        }
+    }
+}
